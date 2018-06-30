d21826bf05a1f8720b26db73c3fe1889409a53d9
David Mortiboy <accounts@davidmortiboy.com>
2018-06-30 14:34:56 +0100

    Set-up initial project files including a unit test project with a simple GET test

---
+++ WebAPI.Tests/MoviesTests.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+
+namespace WebAPI.Tests
+{
+    [TestFixture]
+    public class MoviesTests
+    {
+        [Test]
+        public async Task MoviesTest_GetRequest_Returns200Response()
+        {
+            //Arrange
+            var controller = new MoviesController();
+            //Act
+            var getResponse = controller.Get();
+            //Assert
+            getResponse.Should().BeOfType(typeof(OkObjectResult));
+        }
+    }
+}
---
+++ WebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        // GET api/movies
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "They Live", "Prince of Darkness" });
+        }
+
+        // GET api/movies/1
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/movies
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/movies/1
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/movies/1
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
